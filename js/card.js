7de5959f898849165c9dec95fc1814b88c1f48ed
Fiona Feng <fiona.feng@pearson.com>
2020-04-05 16:18:49 -0400

    initial commit

---
+++ js/card.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el:"#card",
+
+    data:{
+        cards:[
+            {
+                title:"website 1",
+                text:"demo1"
+            },
+            {
+                title:"website 2",
+                text:"demo2"
+            }
+        ]
+    }
+})
